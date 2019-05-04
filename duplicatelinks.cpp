50edf567b9a8dcb724859435e33890bdfbca9870
root <root@DefaultW10.localdomain>
2019-05-04 07:53:40 -0700

    Sample lists

---
+++ duplicatelinks.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include "list.h"
+#include "linkedlist.h"
+
+struct Node 
+{ 
+	int data; 
+	Node * next; 
+}; 
+
+struct Node * newNode (int data)  
+	{
+		Node *temp = new Node; 
+		temp->data = data; 
+		temp->next = nullptr; 
+		return temp; 
+	} ; 
+void RemoveDuplicates(struct Node *start) 
+{ 
+	struct Node *ptr1 , *ptr2, *dup; 
+
+	ptr1 = start; 
+
+
+	while (ptr1 != nullptr && ptr1->next != nullptr) 
+	{ 
+		ptr2 = ptr1; 
+
+		// compare elements
+		while (ptr2->next != nullptr) 
+		{ 
+			if (ptr1->data == ptr2->next->data) 
+			{ 
+
+			dup = ptr2->next; 
+			ptr2->next = ptr2->next->next; 
+			delete (dup); 
+			} 
+			else 
+				ptr2 = ptr2->next; 
+		} 
+		ptr1 = ptr1->next; 
+	}
+} 
+
+using namespace std; 
+
+void PrintList(struct Node *node) 
+{ 
+	while (node != nullptr)
+	{ 
+		cout << node->data; 
+		node = node->next; 
+	} 
+}
+
+
+
+
+int main() 
+{ 
+struct Node *start = newNode(10); 
+start->next = newNode(11); 
+start->next->next = newNode(15); 
+start->next->next->next = newNode(15); 
+
+
+RemoveDuplicates(start); 
+PrintList(start); 	
+} 
---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std; 
+struct Node { 
+	char data; 
+	Node *next; 
+} ; 
+	int main() { 
+			
+		Node *head; 
+		Node *neo; 
+		neo->data =  {1,2,3,4}; 
+		
+		
+
+	} 
+
